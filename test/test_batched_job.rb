ec4ad647cf1bed6320cef4bc966e20f8de65b51d
Daniel Johnston <drjohnston@me.com>
2010-10-18 11:47:24 -0500

    Completed a working test.

--- test/test_batched_job.rb
+++ test/test_batched_job.rb
@@ -3,51 +3,40 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'resque'
 require 'resque/plugins/batched_job'
 
-module TestHooks
-
-  def after_batch_celebrate(id, *args)
-    "batch #{id} is complete!"
-  end
-  
-end
-
-class TestJob
+class TestJob # => aka count your toes and put your socks on!
   
   extend Resque::Plugins::BatchedJob
-  extend TestHooks
-  
+
   @queue = :test
   
-  def self.perform(batch_id, arg)
-    sleep 1
+  def self.perform(foot, toe_num)
+    puts "#{toe_num} " + (toe_num==1 ? 'toe' : 'toes')
   end
+
+  def self.after_batch_put_on_sock(foot, toe_num)
+    puts "putting sock on #{foot} foot.."
+  end
+
+end
+
+=begin
+  TODO: understand Test::Unit::TestCase.  kinda sad.
+class BatchedJobTest < Test::Unit::TestCase
   
+  def test_list
+    assert_nothing_raised do
+      Resque::Plugin.lint(Resque::Plugins::BatchedJob)
+    end
+  end
+    
+end
+=end
+%w(left right).each do |foot|
+  i=0
+  10.times { Resque.enqueue(TestJob, foot, i+=1) }  
 end
 
-# class BatchedJobTest < Test::Unit::TestCase
-#   
-#   def test_list
-#     assert_nothing_raised do
-#       Resque::Plugin.lint(Resque::Plugins::BatchedJob)
-#     end
-#   end
-#   
-#   def test_batched_job
-#     for i in 1000..1010 do
-#       4.times { Resque.enqueue(TestJob, i) }
-#     end
-#     worker = Resque::Worker.new(:test)
-#     worker.work
-#   end
-#   
-# end
-
-for i in 1000..1001 do
-  puts i
-  4.times { puts "enqueue => #{i}"; Resque.enqueue(TestJob, i, rand(1000)) }
-end
 worker = Resque::Worker.new(:test)
 worker.verbose = true
-worker.very_verbose = true
+# worker.very_verbose = true
 worker.work
-
