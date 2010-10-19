d838798a5e35315dd00386d5ab97cf26092e48fc
Daniel Johnston <drjohnston@me.com>
2010-10-19 18:22:20 -0500

    initial import.  examples rock.

---
+++ example/job.rb
@@ -0,0 +1,21 @@
+class Job
+  
+  extend Resque::Plugins::BatchedJob
+  
+  @queue = :example
+  
+  def self.before_perform_turn_computer_off(place, name)
+    puts "#{name} straightening up desk"
+  end
+  
+  def self.perform(place, name)
+    puts "#{name} leaving the #{place}"
+    sleep 1
+  end
+  
+  def self.after_batch_turn_lights_out(place, name)
+    puts "#{name} turing the lights out in the #{place}."
+    sleep 1
+  end
+  
+end
