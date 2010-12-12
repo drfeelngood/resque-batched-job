b0fdabbea51987505dbe9e8fd61ebd8e6f8b98a1
drfeelngood <drjohnston@me.com>
2010-12-11 20:43:29 -0600

    Added test methods

--- test/test_batched_job.rb
+++ test/test_batched_job.rb
@@ -1,6 +1,28 @@
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
+require 'test/unit'
 require 'resque'
+
+$:.unshift(File.dirname(__FILE__) + '../lib')
+
 require 'resque/plugins/batched_job'
+
+class Job
+  
+  extend Resque::Plugins::BatchedJob
+  
+  @queue = :test
+  
+  def self.perform(batch_id)
+    $stdout.puts "perform batch[#{batch_id}]"
+    sleep 1
+  end
+  
+  def self.after_batch_hook(batch_id)
+    $stdout.puts "after_bactch [#{batch_id}]"
+    sleep 1
+  end
+  
+end
 
 class BatchedJobTest < Test::Unit::TestCase
   
@@ -8,6 +30,16 @@ class BatchedJobTest < Test::Unit::TestCase
     assert_nothing_raised do
       Resque::Plugin.lint(Resque::Plugins::BatchedJob)
     end
+  end
+  
+  def test_version
+    assert_equal('1.10.0', Resque::Version)
+  end
+  
+  def test_batched_job
+    5.times { Resque.enqueue(Job, 1, "arg#{rand(100)}") }
+    worker = Resque::Worker.new(:test)
+    worker.work
   end
     
 end
