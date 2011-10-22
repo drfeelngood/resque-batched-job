9b43b010dabbd3039bdd16dd1789230d1f9d0404
drfeelngood <dan@dj-agiledev.com>
2011-10-22 12:54:15 -0500

    Re worked test unit.

--- test/test_batched_job.rb
+++ test/test_batched_job.rb
@@ -1,30 +1,35 @@
 require 'rubygems'
 require 'test/unit'
 require 'resque'
-
-$:.unshift(File.dirname(__FILE__) + '../lib')
-
-require 'resque/plugins/batched_job'
+require 'resque-batched-job'
 
 class Job
-  
   extend Resque::Plugins::BatchedJob
-  
   @queue = :test
   
-  def self.perform(batch_id)
-    $stdout.puts "perform batch[#{batch_id}]"
-    sleep 1
+  def self.perform(batch_id, arg)
   end
   
-  def self.after_batch_hook(batch_id)
-    $stdout.puts "after_bactch [#{batch_id}]"
-    sleep 1
+  def self.after_batch_hook(batch_id, arg)
+    $batch_complete = true
   end
   
 end
 
 class BatchedJobTest < Test::Unit::TestCase
+  
+  def setup
+    $batch_complete = false
+    @batch_id = :foo
+    @batch = "batch:#{@batch_id}"
+    @cnt = 5
+    @cnt.times { Resque.enqueue(Job, @batch_id, "arg#{rand(100)}") }
+  end
+  
+  def teardown
+    redis.del(@batch)
+    redis.del("queue:test")
+  end
   
   def test_list
     assert_nothing_raised do
@@ -32,14 +37,26 @@ class BatchedJobTest < Test::Unit::TestCase
     end
   end
   
-  def test_version
-    assert_equal('1.10.0', Resque::Version)
+  def test_batch_key
+    assert_equal(@batch, Job.batch(@batch_id))
   end
   
-  def test_batched_job
-    5.times { Resque.enqueue(Job, 1, "arg#{rand(100)}") }
-    worker = Resque::Worker.new(:test)
-    worker.work
+  def test_batch_size
+    assert_equal(@cnt, redis.smembers(@batch).size)
   end
+  
+  def test_batch_hook
+    assert_nothing_raised do
+      @cnt.times { Resque.reserve(:test).perform }
+    end
+    assert($batch_complete)
+    assert_equal(false, redis.exists(@batch))
+  end
+
+  private
+
+    def redis
+      Resque.redis
+    end
     
 end
