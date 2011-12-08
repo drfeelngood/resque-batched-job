a1b613f4381662c937862e96f9df0d18367f39f1
drfeelngood <dan@dj-agiledev.com>
2011-12-08 15:37:11 -0600

    Updated how batched job entries are encoded.  They are now encoded the same as the original job entry.  I also did a little white space cleaning.

--- lib/resque/plugins/batched_job.rb
+++ lib/resque/plugins/batched_job.rb
@@ -1,33 +1,30 @@
 module Resque
-  
+
   module Plugin
-    
-    #
+
     # This hook is really the meaning of our adventure.
     def after_batch_hooks(job)
       job.methods.grep(/^after_batch/).sort
     end
-    
+
   end
-  
+
   module Plugins
-    
+
     module BatchedJob
-    
+
       include Resque::Helpers
 
-      #
       # Helper method used to generate the batch key.
       def batch(id)
         "batch:#{id}"
       end
 
-      #
       # Batch the job.  The first argument of a batched job, is the batch id.
       def after_enqueue_batch(id, *args)
-        redis.sadd(batch(id), encode(args.insert(0, self.name)))
+        redis.sadd(batch(id), encode(:class => self, :args => args))
       end
-      
+
 =begin
   TODO: Determine if it's necessary to double check the jobs existance 
     before performing it.  If so, do what?
@@ -39,19 +36,15 @@ module Resque
       end
 =end
 
-      # 
-      #   After every job, no matter in the event of success or failure, we need
+      # After every job, no matter in the event of success or failure, we need
       # to remove the job from the batch set.
       def around_perform_amend_batch(id, *args)
-        begin
-          yield
-        ensure
-          redis.srem(batch(id), "#{encode(args.insert(0, self.name))}")
-        end
+        yield
+      ensure
+        redis.srem(batch(id), "#{encode(:class => self, :args => args)}")
       end
 
-      #
-      #   After each job is performed, check to see if the job is the last of
+      # After each job is performed, check to see if the job is the last of
       # the given batch.  If so, run after_batch hooks.
       def after_perform_batch(id, *args)
         if batch_complete?(id)
@@ -61,18 +54,17 @@ module Resque
           end
         end
       end
-      
-      #
-      #   Checks to see if the batch key exists.  If the key does exist, is the 
+
+      # Checks to see if the batch key exists.  If the key does exist, is the 
       # set empty?  The Redis srem command deletes the key when the last item 
       # of a set is removed. Ah, go ahead and check the size.
       def batch_complete?(id)
         redis.scard(batch(id)) == 0
       end
       alias :batch_exist? :batch_complete? # => are they?
-      
+
     end
-    
+
   end
-  
+
 end
